bee3fbaae629c7412a9a9e8d9d066e68816286a9
DavidMerchanMartinez <merchandavid1632@gmail.com>
2023-06-13 11:05:54 -0500

    creacion campos de proyectos

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,26 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Portafolio.Models;
 using System.Diagnostics;
+using Portafolio.Servicios;
 
 namespace Portafolio.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IRepositorioProyectos repositorioProyectos;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IRepositorioProyectos repositorioProyectos)
         {
             _logger = logger;
+            this.repositorioProyectos = repositorioProyectos;
         }
 
         public IActionResult Index()
         {
-            return View();
+         
+            var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();
+            var modelo = new HomeIndex() { Proyectos = proyectos };
+            return View(modelo);
         }
 
-        public IActionResult Privacy()
+        public IActionResult Proyectos()
         {
-            return View();
+            var proyectos = repositorioProyectos.ObtenerProyectos();
+            return View(proyectos);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Servicios/RepositorioProyectos.cs
@@ -0,0 +1,46 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+    public interface IRepositorioProyectos
+    {
+        List<ProyectoDTO> ObtenerProyectos();
+    }
+    public class RepositorioProyectos: IRepositorioProyectos
+    {
+        public List<ProyectoDTO> ObtenerProyectos()
+        {
+            return new List<ProyectoDTO>()
+            {
+                new ProyectoDTO
+                {
+                    Titulo = "Amazon",
+                    Descripcion = "E-commerce realizado en ASP.NET Core",
+                    Link =  "https://amazon.com",
+                    ImagenURL = "/imagenes/prueba.png"
+                },
+                new ProyectoDTO
+                {
+                    Titulo = "New York Times",
+                    Descripcion = "Pagina de noticias en react",
+                    Link =  "https://nytimes.com",
+                    ImagenURL = "/imagenes/prueba.png"
+                },
+                new ProyectoDTO
+                {
+                    Titulo = "Reddit",
+                    Descripcion = "Red social para compartir en comunidades",
+                    Link =  "https://reddit.com",
+                    ImagenURL = "/imagenes/prueba.png"
+                },
+                new ProyectoDTO
+                {
+                    Titulo = "steam",
+                    Descripcion = "tienda en linea para comprar videojuegos",
+                    Link =  "https://store.steampowered.com",
+                    ImagenURL = "/imagenes/prueba.png"
+                },
+            };
+        }
+    }
+}
